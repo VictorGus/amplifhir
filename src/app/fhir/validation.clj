b5286fbf2420692193d76032fab3f468e3a37a15
VictorGus <retaow@gmail.com>
2021-04-03 22:29:00 +0300

    init validation

--- src/app/fhir/validation.clj
+++ src/app/fhir/validation.clj
@@ -1,4 +1,29 @@
-(ns app.fhir.validation)
+(ns app.fhir.validation
+  (:require [scjsv.core :as v]))
 
+(defn validate-uniqueness [ctx resource]
+  )
+
+(defn validate-references [])
+
+(defn validate-resource [])
 
 ;;TODO
+
+(comment
+
+  (def schema {:$schema "http://json-schema.org/draft-04/schema#"
+               :type "object"
+               :properties {:id   {:type "integer"}
+                            :test {:type "string"}}
+               :required [:id]})
+
+  (def validate (v/validator schema))
+
+  (validate {:id 2})
+
+
+
+
+  )
+
