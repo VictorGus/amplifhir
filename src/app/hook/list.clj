4cf03caf8c5cfe93d2345c5c900ca49807ad6c14
VictorGus <retaow@gmail.com>
2021-04-11 20:43:19 +0300

    keep order of hooks

--- src/app/hook/list.clj
+++ src/app/hook/list.clj
@@ -1,7 +1,9 @@
-(ns app.hook.list)
+(ns app.hook.list
+  (:require [app.fhir.validation :as validation]))
 
+;;NOTE: the order is important. 1 - authorization 2 - validation 3 - history
+;;NOTE: hooks must return nil or error (actual http response)
 (def hooks
-  {:validation-hook {:trigger {:operations [:update :create]
-                               :resources :fhir}
-                     :handler (fn [ctx req]
-                                (println "test"))}})
+  (array-map :validation-hook {:trigger {:operations [:update :create]
+                                         :resources :fhir}
+                               :handler validation/validation-hook}))
