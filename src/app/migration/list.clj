df30bfe78f3cadb07ec526c6e8be918216391247
VictorGus <retaow@gmail.com>
2021-04-25 20:30:24 +0300

    add migration for root user & client

--- src/app/migration/list.clj
+++ src/app/migration/list.clj
@@ -1,6 +1,7 @@
 (ns app.migration.list
   (:require [app.migration.search-param]
-            [app.fhir.validation]))
+            [app.migration.subject]))
 
 (def migrations
-  {:text-index         app.migration.search-param/text-index-migration})
+  {:text-index         app.migration.search-param/text-index-migration
+   :super-subjects     app.migration.subject/auth-super-migration})
--- src/app/migration/subject.clj
+++ src/app/migration/subject.clj
@@ -1,10 +1,18 @@
 (ns app.migration.subject
-  (:require [cheshire.core   :as json]
-            [app.rest.utils  :as u]
-            [app.db.core     :as db]
-            [clojure.java.io :as io]
-            [clojure.string  :as str]))
+  (:require [cheshire.core    :as json]
+            [app.rest.utils   :as u]
+            [app.db.core      :as db]
+            [app.auth.subject :as s]
+            [clojure.java.io  :as io]
+            [clojure.string   :as str]))
 
-(defn auth-subject-migration [{conn :db/connection :as ctx}]
-  (db/ensure-index conn rn exps (str (name rn) "_text")))
+(defn auth-super-migration
+  [{conn :db/connection {{super-user   :super_user
+                          super-client :super_client} :auth} :modules :as ctx}]
+  (let [{client-id :_id :as enriched-client} (s/shape-super super-client)
+        {user-id   :_id  :as enriched-user}   (s/shape-super super-user)]
+    (db/create conn :Client {:_id client-id
+                             :resource (dissoc enriched-client :_id)})
+    (db/create conn :User   {:_id user-id
+                             :resource (dissoc enriched-user :_id)})))
 
